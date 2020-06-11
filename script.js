8b9a34ebeddefee07fef29ea9d4a1f635b774381
Austin Calvelage <austin.calvelage@icloud.com>
2020-06-10 23:08:12 -0400

    Refactored some code

--- script.js
+++ script.js
@@ -21,42 +21,53 @@ const possibleAnswers = [
   `Outlook not so good.`,
   `Very doubtful.`,
 ];
+//
+// let clicked;
 
+// Adding html elements to JavaScript variables
 const htmlAnswer = document.querySelector('.answer');
 const submitBtn = document.querySelector('.submitBtn');
 const resetBtn = document.querySelector('.resetBtn');
-
+const question = document.querySelector('#question-input');
 const ball = document.querySelector('.ball');
 const answerDisplay = document.querySelector('.logo-container');
 
+// Picks a random value from array between 0-19
 function giveAnswer() {
   const fortune = possibleAnswers[Math.floor(Math.random() * 20)];
   htmlAnswer.insertAdjacentText('afterbegin', fortune);
 }
 
+// Adds shake class to ball figure
 function animate() {
   ball.classList.toggle('shake');
 }
 
+// Switches logo container to hidden and unhides answer container
 function getAnswer() {
   answerDisplay.classList.toggle('logo-container');
   answerDisplay.classList.toggle('answer-container');
   giveAnswer();
 }
 
-let clicked = 0;
-
 submitBtn.onclick = function() {
-  clicked = true;
-  animate();
-  getAnswer();
-  // on click hide submit button and unhide reset button
-  if (clicked === true) {
-    submitBtn.classList.add('hide');
-    resetBtn.classList.toggle('unhide');
+  // Checks if input field is blank and outputs an alert if so.
+  if (question.value == false) {
+    alert('ENTER A question');
+    // If valid input runs rest of program
+  } else {
+    // declares let and sets it to true on click of submit
+    const clicked = true;
+    animate();
+    getAnswer();
+    // on click hide submit button and unhide reset button
+    if (clicked === true) {
+      submitBtn.classList.add('hide');
+      resetBtn.classList.toggle('unhide');
+    }
   }
-};
-// on button click reload page
-resetBtn.onclick = function() {
-  location.reload();
+  // on click of reset-button reload page
+  resetBtn.onclick = function() {
+    location.reload();
+  };
 };
